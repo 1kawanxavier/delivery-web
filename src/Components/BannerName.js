e5342abef1bef2838a1876af0354ac566497e116
Kawan Xavier <kawanxavier1818@gmail.com>
2024-04-24 19:46:12 -0300

    first commit

---
+++ src/Components/BannerName.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function BannerName({ name, discount, more }) {
+  const currency = "$";
+  return (
+    <div className="bannerContent">
+      <h3>Oi Kawan e Arthur,</h3>
+      <p>
+          Já que o BK deu lanche pra os calvos , integrantes do senai ganha Pizza aqui
+      </p>
+      <a href={more}>Saiba mais</a>
+    </div>
+  );
+}
+
+export default BannerName;
